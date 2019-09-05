a09b7320e4536b127b4eeba15b2b95d698beb4a3
Martin <martin.kozon12@gmail.com>
2019-09-05 11:51:12 +0100

    Web Design + Python course

---
+++ Python/sum_of_two.py
@@ -0,0 +1,8 @@
+#Program which will add two numbers together
+
+#User has to input two numbers - number1 and number2
+number1 = int(input("Number a: "))
+number2 = int(input("Number b: "))
+
+#add numbers number1 and number2 together and print it out
+print(number1 + number2)
