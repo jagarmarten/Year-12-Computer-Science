aa77f408a8a64330e2ef8183cf1f6469c82e6de6
Martin <martin.kozon12@gmail.com>
2019-09-06 12:15:34 +0100

    Create greatest_num_out_of_3.py

---
+++ Python/greatest_num_out_of_3.py
@@ -0,0 +1,16 @@
+#input three numbers, compare them and print out the larger one
+number1 = int(input("First number: ")) #first number choice
+number2 = int(input("Second number: ")) #second number choice
+number3 = int(input("Third number: ")) #third number choice
+
+#if the number1 is greater than number2, return number1
+#otherwise return number2
+
+if (number1 > number2 and number1 > number3):
+    print(str(number1))
+elif (number2 > number1 and number2 > number3):
+    print(str(number2))
+elif (number3 > number1 and number3 > number2):
+    print(str(number3))
+else:
+    print(str(number1))
