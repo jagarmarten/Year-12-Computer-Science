3708aae3a0cb3c8daa6938e8c99a697a3f1f5df9
Martin <martin.kozon12@gmail.com>
2019-09-06 11:45:53 +0100

    assignment

--- Python/times_table.py
+++ Python/times_table.py
@@ -4,12 +4,16 @@
 #ask a user to input a number
 number = int(input("Input number between 1 and 10: "))
 
+
+while number<1 or number>10:
+    number = int(input("Input number between 1 and 10: "))    
+
 #check whether the number is smaller or equal to 10 or greater or equal to 1
 #if it's 
 if number >=10 or number <=1:
     print("Number needs to be BETWEEN 1 and 10!")
 else:
     #if the input number fits, do a for loop
-    for x in range(number):
+    for x in range(12):
         table = (x+1) * number #multiply x by the inputted number
         print(str(x + 1) + " * " + str(number) + " = " + str(table)) #print out a result
