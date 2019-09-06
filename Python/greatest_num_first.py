8f0233e785d0c0efd0215a611cb6003fdfbec26a
Martin <martin.kozon12@gmail.com>
2019-09-06 12:07:47 +0100

    Create greatest_num_first.py

---
+++ Python/greatest_num_first.py
@@ -0,0 +1,10 @@
+#input two numbers, compare them and print the larger one
+number1 = int(input("First number: ")) #first number choice
+number2 = int(input("Second number: ")) #second number choice
+
+#if the number1 is greater than number2, return number1
+#otherwise return number2
+if number1 > number2:
+    print(str(number1))
+else:
+    print(str(number2))
