d8f906234d686a58cc75f58afebbfc48ba21316b
Martin <martin.kozon12@gmail.com>
2019-09-09 15:38:33 +0100

    updates

---
+++ Python/06_input-validation.py
@@ -0,0 +1,22 @@
+#User has to input a number between 1 and 10
+#eg. if user inputs 3, the result will be as follows: 3 -> 3*1=3, 3*2=6, 3*3=9
+
+#ask a user to input a number
+number = int(input("Input number between 1 and 10: "))
+
+#if the input number is 99 than exit the program
+if number == 99:
+    exit()
+# end if
+
+#if the number isn't the one we want, the user will have to submit it again
+while number < 1 or number > 10:
+    print("Number needs to be BETWEEN 1 and 10")
+    number = int(input("Input number between 1 and 10: "))
+# end while
+
+#times table running in a for loop with a range of 12
+for x in range(12):
+    table = (x+1) * number #multiply x by the inputted number
+    print(str(x + 1) + " * " + str(number) + " = " + str(table)) #print out a result
+# end for
