defef97a8308e3125304ddf6dd71d19cef719f22
Martin <martin.kozon12@gmail.com>
2019-10-19 23:07:39 +0200

    Update pong_4.py

--- Python/PyGame/pong_4.py
+++ Python/PyGame/pong_4.py
@@ -25,15 +25,17 @@ done = False
 clock = pygame.time.Clock()
 
 ball_width = 20
+paddle_width = 15
+paddle_height = 60
 
-x_val = 150
-y_val = 200
+x_val = 250
+y_val = 150
 
 x_pad = 0
 y_pad = 0
 
-x_direction = 3
-y_direction = 3
+x_direction = -1
+y_direction = 0
 
 # -- Game Loop
 while not done:
@@ -42,16 +44,23 @@ while not done:
         if event.type == pygame.QUIT:
             done = True
         # End If
-    keys = pygame.key.get_pressed()
+    # End for
+
     ## - the up key or down key has been pressed
+    keys = pygame.key.get_pressed()
     if keys[pygame.K_UP]:
         y_pad = y_pad - 5
         if y_pad < 0:
             y_pad = 0
+        #end if
+    #end if
     if keys[pygame.K_DOWN]:
         y_pad = y_pad + 5
         if y_pad > size[1] - 60:
             y_pad = size[1] - 60
+        #end if
+    #end if
+
     # -- Next event
     # -- Game logic goes after this comment
     x_val = x_val + x_direction
@@ -59,23 +68,26 @@ while not done:
 
     if y_val > (size[1] - ball_width) or y_val < (0):
         y_direction = y_direction * -1
-
+    #end if
     if x_val > (size[0] - ball_width) or x_val < (0):
         x_direction = x_direction * -1
-
+    #end if
     #changing the direction of the ball once it hits the paddle
-    if x_val <= 15 and y_val >= y_pad - 19 and y_val <= y_pad + 19 and y_val <= y_pad + 60:
+    #if x_val <= 15 and y_val >= y_pad and y_val <= y_pad + 60:
+    #    y_direction = y_direction * -1
+    if x_val <= 15 and y_val >= y_pad and y_val <= y_pad + 60:
         x_direction = x_direction * -1
-
+    #if x_val >= 0 and x_val <= 15
+    #end if
     if x_pad > 0 and y_pad > 0:
         y_pad = 0
         x_pad = 0
-
+    #end if
     # -- Screen background is BLACK
     screen.fill(BLACK)
     # -- Draw here
     pygame.draw.rect(screen, BLUE, (x_val, y_val, ball_width, ball_width))
-    pygame.draw.rect(screen, WHITE, (x_pad, y_pad, 15, 60))
+    pygame.draw.rect(screen, WHITE, (x_pad, y_pad, paddle_width, paddle_height))
     # -- flip display to reveal new position of objects
     pygame.display.flip()
     # -- The clock ticks over
