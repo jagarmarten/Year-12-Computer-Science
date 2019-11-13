cf3e60f411a70916a8eb8fa0189986ecd8064096
Martin <martin.kozon12@gmail.com>
2019-11-13 23:30:26 +0000

    Update balls.py

--- Python/PyGame/balls.py
+++ Python/PyGame/balls.py
@@ -1,4 +1,6 @@
 import pygame
+import math
+import random
 # -- Global Constants
 
 # -- Colours
@@ -31,23 +33,35 @@ clock = pygame.time.Clock()
 
 
 class Ball():
-
-    def __init__(self, x, y, col):
+    def __init__(self, x, y, x_dir, y_dir, col):
         self.color = col
-        self.x_coord = 0
-        self.y_coord = 0
-        self.size = 10
+        self.x_coord = x
+        self.y_coord = y
+        self.x_dir = x_dir
+        self.y_dir = y_dir
+        self.size = 5
 
-    def draw(self):
-        pygame.draw.circle(screen, snow_flake.color,
-                           (snow_flake.x_coord, snow_flake.y_coord), snow_flake.size)
+    def draw(self, screen):
+        pygame.draw.circle(screen, self.color, (self.x_coord, self.y_coord), self.size) #getting error when calling screen variable
 
     def move(self):
-        self.x_coord += 2
-        self.y_coord += 2
+        self.x_coord = self.x_coord + self.x_dir
+        self.y_coord = self.y_coord + self.y_dir
+        #changing the direction of the ball object if it hits the edge of the screen
+        if self.x_coord < 0 + self.size or self.x_coord > 640 - self.size:
+            self.x_dir *= -1
+        if self.y_coord < 0 + self.size or self.y_coord > 480 - self.size:
+            self.y_dir *= -1
 
+#list of balls - their x val, y val, and color
+#ball_list = [[100, 100, WHITE], [200, 100, YELLOW], [300, 150, BLUE], [400, 180, BLUE]]
+ball_color = [WHITE, BLUE, YELLOW]
+balls = [] #balls will be a list of Ball() objects
 
-snow_flake = Ball(100, 100, WHITE)
+for ball_data in range(150):
+    #my_ball = Ball(random.randint(0,400), random.randint(0,400), BLUE)
+    my_ball = Ball(random.randint(0, 640), random.randint(0,480), 1, 1, ball_color[random.randint(0, len(ball_color) - 1)])
+    balls.append(my_ball) #add the new randomly generated 
 
 ### -- Game Loop
 while not done:
@@ -61,8 +75,10 @@ while not done:
     # -- Screen background is BLACK
     screen.fill(BLACK)
 
-    snow_flake.draw()
-    snow_flake.move()
+    #select a ball from balls list - a list of ball objects
+    for ball in balls:
+        ball.draw(screen) #draw the ball
+        ball.move() #move the ball
 
     pygame.display.flip()
     # - The clock ticks over
