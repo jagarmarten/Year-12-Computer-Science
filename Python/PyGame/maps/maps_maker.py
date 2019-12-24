7cd3af39ce667204527cf4b45b7c3fcc1aed972a
Martin <martin.kozon12@gmail.com>
2019-12-25 00:13:07 +0100

    final touches to the map_maker.py
    If you want to create your own grid for maps game, you'll have to do following
    #  1. Once you execute the code, you'll be able to see a grid - 32 by 32
    #  2. Left click if you want to create a new wall,
    #     Right click if you want to remove a wall
    #  3. Python will automaticaly create and update a new .txt file for you. The file is called "map_export.txt" and that is where you'll be able to find your map array.

--- Python/PyGame/maps/maps_maker.py
+++ Python/PyGame/maps/maps_maker.py
@@ -1,3 +1,10 @@
+# If you want to create your own grid for maps game, you'll have to do following
+#  1. Once you execute the code, you'll be able to see a grid - 32 by 32
+#  2. Left click if you want to create a new wall,
+#     Right click if you want to remove a wall
+#  3. Python will automaticaly create and update a new .txt file for you. The file is called "map_export.txt" and that is where you'll be able to find your map array
+#
+
 import random
 import math
 import pygame
@@ -8,6 +15,10 @@ BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 BLUE = (50, 50, 255)
 YELLOW = (255, 255, 0)
+
+# Defining mouse click values
+LEFT = 1
+RIGHT = 3
 
 # -- Initialise PyGame
 pygame.init()
@@ -67,7 +78,7 @@ map = [
 ## -- Define the class tile which is a sprite
 class Tile(pygame.sprite.Sprite):
     # Define the constructor for invader
-    def __init__(self, color, width, height, x_ref, y_ref, wall_color):
+    def __init__(self, color, width, height, x_ref, y_ref):
         # Call the sprite constructor
         super().__init__()
         self.image = pygame.Surface([width, height])
@@ -75,7 +86,6 @@ class Tile(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = x_ref
         self.rect.y = y_ref
-        self.wall_color = wall_color
 
 # Create a list of all sprites
 all_sprites_list = pygame.sprite.Group()
@@ -83,15 +93,28 @@ all_sprites_list = pygame.sprite.Group()
 wall_list = pygame.sprite.Group()
 
 # Create walls on the screen (each tile is 20 x 20 so alter cords)
-for y in range(32):
-    for x in range(32):
-        if map[x][y] == 1:
-            my_wall = Tile(BLUE, 20, 20, x*20, y * 20, WHITE)
-            wall_list.add(my_wall)
-            all_sprites_list.add(my_wall)
-        #end if
+def create_boxes():
+    for y in range(32):
+        for x in range(32):
+            if map[x][y] == 1:
+                my_wall = Tile(BLUE, 20, 20, x*20, y * 20)
+                all_sprites_list.add(my_wall)
+            else:
+                my_wall = Tile(BLACK, 20, 20, x*20, y * 20)
+                all_sprites_list.add(my_wall)
+            #end if
+        #end for
     #end for
-#end for
+    grid()
+
+def grid():
+    # Creating the visual representation of the Grid (20 by 20px - 32 rows and collumns)
+    #vertical grid
+    for x in range(33):
+        all_sprites_list.add(Line(WHITE, [x * 20 - 0.5, 0], 1, 640, 1))
+    #horizontal grid
+    for y in range(33):
+        all_sprites_list.add(Line(BLUE, [0, y * 20 - 0.5], 640, 1, 1))
 
 class Line(pygame.sprite.Sprite):
     # Define the constructor for invader
@@ -104,27 +127,38 @@ class Line(pygame.sprite.Sprite):
         self.rect.x = start[0]
         self.rect.y = start[1]
 
-# Creating the visual representation of the Grid (20 by 20px - 32 rows and collumns)
-#vertical grid
-for x in range(33):
-    all_sprites_list.add(Line(WHITE, [x * 20 - 0.5, 0], 1, 640, 1))
-#horizontal grid
-for y in range(33):
-    all_sprites_list.add(Line(BLUE, [0, y * 20 - 0.5], 640, 1, 1))
 
+grid()
 ### -- Game Loop
 while not done:
     # -- User input and controls
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
-        if event.type == pygame.MOUSEBUTTONDOWN:
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == LEFT:
             mouse_pos_x, mouse_pos_y = pygame.mouse.get_pos()
             column = mouse_pos_x // 20
             row = mouse_pos_y // 20
             #print(column, row)
             map[column][row] = 1
-            print(map)
+            # Create a new file map_export.txt
+            file = open("map_export.txt", "w+")
+            # Write the map inside of the file
+            file.write(str(map))
+            # Create walls on the screen (each tile is 20 x 20 so alter cords)
+            create_boxes()
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == RIGHT:
+            mouse_pos_x, mouse_pos_y = pygame.mouse.get_pos()
+            column = mouse_pos_x // 20
+            row = mouse_pos_y // 20
+            #print(column, row)
+            map[column][row] = 0
+            # Create a new file map_export.txt
+            file = open("map_export.txt", "w+")
+            # Write the map inside of the file
+            file.write(str(map))
+            # Create walls on the screen (each tile is 20 x 20 so alter cords)
+            create_boxes()
     #next event
     all_sprites_list.update()
     # -- Screen background is BLACK
