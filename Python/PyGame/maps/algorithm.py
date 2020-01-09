0a8b87029a27c048120157c1459d24c2ad39472d
Martin <martin.kozon12@gmail.com>
2020-01-09 09:01:25 +0000

    Update algorithm.py

--- Python/PyGame/maps/algorithm.py
+++ Python/PyGame/maps/algorithm.py
@@ -1,3 +1,6 @@
+from math import sqrt #importing the sqrt function from math
+
+#defining the grid we'll be working with
 grid = [
     [0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0],
@@ -12,5 +15,11 @@ def path (start, end):
     end_x, end_y = end
     grid[start_x][start_y] = 1 #number 1 represents the initial position
     grid[end_x][end_y] = 2 #number 2 represents the final destination
+    
+    found = False #will be set to True once the number is found
+    path = 0 #this will be the distance in the grid
+
+    air_distance = sqrt((start_x - end_x) ** 2 + (start_y - end_y) ** 2) #the distance "by air"
+    print(distance)
 
 path((0,0), (3,4))
