a68012a6072e70d51591bd30f1a001dc67f0bae8
Martin Kozon <martin.kozon12@gmail.com>
2020-11-03 15:04:20 +0000

    updates

---
+++ A-level project - Skynet Rebellion game/game.py
@@ -0,0 +1,6 @@
+# -- Import and initialize the pygame library
+import pygame  # - Import pygame library
+from pygame.locals import *
+pygame.init()  # - Initialize the pygame library
+
+screen = pygame.display.set_mode([800, 600])
