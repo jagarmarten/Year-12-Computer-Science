df12c0464c825a09bd07ecd29350e935fd74e05e
Martin Kozon <martin.kozon12@gmail.com>
2020-11-03 15:11:13 +0000

    Iteration 1
    Commit 001
    
    The first steps towards the development of the game.

--- A-level project - Skynet Rebellion game/game.py
+++ A-level project - Skynet Rebellion game/game.py
@@ -1,6 +1,6 @@
-# -- Import and initialize the pygame library
-import pygame  # - Import pygame library
-from pygame.locals import *
-pygame.init()  # - Initialize the pygame library
+# - GAME.PY
+import pygame  # -- import the pygame library
+from pygame.locals import * # -- pygame.locals is a module that contains various pygame constants
+pygame.init()  # -- initialize the pygame library
 
-screen = pygame.display.set_mode([800, 600])
+screen = pygame.display.set_mode([800, 600]) # -- create a screen with a resolution of 800 by 600
