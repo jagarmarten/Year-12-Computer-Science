845070f029458b4a0cfb4a80204be5f2c2f182f0
Martin Kozon <martin.kozon12@gmail.com>
2020-11-05 10:05:36 +0000

    Initial setup + first screenshot

--- A-level project - Skynet Rebellion game/game.py
+++ A-level project - Skynet Rebellion game/game.py
@@ -4,3 +4,24 @@ from pygame.locals import * # -- pygame.locals is a module that contains various
 pygame.init()  # -- initialize the pygame library
 
 screen = pygame.display.set_mode([800, 600]) # -- create a screen with a resolution of 800 by 600
+
+running = True # -- variable 'running' is used to stop the game if the user closes the window
+
+# - MAIN WHILE LOOP
+# -- this is where the game is being executed
+while running:
+    
+    # - FOR loop which listens to events
+    for event in pygame.event.get():
+        # -- if user quits the game
+        if event.type == pygame.QUIT:
+            running = False # -- set the variable running to False
+        # - END IF
+    # - END FOR
+
+    screen.fill((255, 255, 255)) # -- fill the screen with white colour
+
+    pygame.display.flip() # -- Flip the display
+# - END WHILE
+
+pygame.quit() # - Exit the game
