fc8f8bbcef99f74b2e311d4f09d650c7af591e68
Martin Kozon <martin.kozon12@gmail.com>
2020-11-30 02:09:48 +0000

    comment added

--- A-level project - Skynet Rebellion game/game.py
+++ A-level project - Skynet Rebellion game/game.py
@@ -102,7 +102,8 @@ while running:
         player.player_on_platform = True # -- Set the player_on_platform to True
     else:
         player.player_on_platform = False # -- Set the player_on_platform to True
-
+    # -- END IF
+    
     pygame.display.flip() # -- Flip the display
     pygame.time.Clock().tick(60) # -- set the number of frames per second to 60
 # - END WHILE
